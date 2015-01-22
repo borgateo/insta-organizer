530184041fe52065a57eb9b1210aaf60020de02c
Matteo Borgato <borgato.matteo@gmail.com>
2015-01-22 01:33:27 +0000

    token secret update

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   db: process.env.db || 'localhost',
   clientSecret: process.env.clientSecret || '71c32a5a389a41e6831322e392804ba3',
-  tokenSecret: process.env.tokenSecret || 'pick a hard to guess string'
+  tokenSecret: process.env.tokenSecret || 'this is a very long string for the tokenSecret'
 };
